99e0064e8790a3985bbeb206602fcc3c0e5f37c9
Dylan J. Burton <dylanjburton2@gmail.com>
2020-06-09 10:59:56 -0700

    finish horizontal collision

--- 2d_jumping_physics/jump.py
+++ 2d_jumping_physics/jump.py
@@ -1,7 +1,7 @@
 #IMPROVEMENTS
 # 1 - Player can jump even if below a platform?
-# 2 - horizontal collision
-# 3 -  
+# 2 - horizontal collision - Done 
+# 3 - Put player on platform if just about to make it
 
 import pgzrun
 #define screen size
@@ -11,6 +11,8 @@ HEIGHT = 800
 MAROON = 128,0,0
 #vertical accelerations
 GRAVITY = 0.2
+
+HORIZONTAL_SPEED = 2
 
 platforms = [
   Rect((0,780),(800,20)),
@@ -27,25 +29,27 @@ player.y_velocity = 0
 player.jump_velocity = -7
 
 def update():
-  #horizontal movement
-  if keyboard.left and player.x > 0:
-    player.x -= 2
-  if keyboard.right and player.x < 780:
-    player.x += 2
-  
-  #vertical movement
+  # Collision Check
+  newx = player.x
+
+  x_collision = False
+  if keyboard.left:
+    newx -= HORIZONTAL_SPEED
+    if player.x < 0: x_collision = True
+  if keyboard.right:
+    newx += HORIZONTAL_SPEED
+  new_player_position_x = Rect((newx, player.y),(player.w, player.h))
+
   newy = player.y
-  
   player.y_velocity += GRAVITY
   newy += player.y_velocity
-
   new_player_position_y = Rect((player.x, newy),(player.w, player.h))
 
-  #collision check
   y_collision = False
   for platform in platforms:
     y_collision = new_player_position_y.colliderect(platform) or y_collision
-  
+    x_collision = new_player_position_x.colliderect(platform) or x_collision
+
   if y_collision:
     player.y_velocity = 0
   else:
@@ -54,6 +58,12 @@ def update():
   # jump if colliding on y axis
   if keyboard.space and y_collision: 
     player.y_velocity = player.jump_velocity
+
+  # Move if the player will not collide horizontally
+  if keyboard.left and not x_collision and player.x > 0:
+    player.x = newx
+  elif keyboard.right and not x_collision and player.x < 780:
+    player.x = newx
 
 def draw():
   screen.clear()
